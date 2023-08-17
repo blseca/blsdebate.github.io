fc3fd1d56c2057c14929953ec837de5f2be4693b
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2023-08-17 15:11:20 -0400

    new debate changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const words = ["Public Forum", "Debate", "Speech", "Congress", "\"website\"", "Emily Yu", "Water"]
-const wordCount = 20
+const words = ["Public Forum", "Debate", "Speech", "Congress", "\"website\"", "Carolineg Song", "Water"]
+const wordCount = window.innerWidth / 40
 window.addEventListener('load',()=>{
     const header = document.querySelector("header")
     for(let i = 0; i < wordCount; i++){
@@ -7,7 +7,7 @@ window.addEventListener('load',()=>{
         p.innerHTML = words[Math.floor(Math.random()*words.length)]
         let depth = Math.random()*15-3
         p.style.transform = `translateZ(${-depth}px)`
-        p.style.translate = `${(Math.random()-0.5)*100*(depth+4)/4}vw ${(Math.random()-0.5)*400*(depth+4)/4}px`
+        p.style.translate = `${(Math.random()-0.5)*100*(depth+4)/4}vw ${(Math.random()*1.3-0.5)*400*(depth+4)/4}px`
         p.style.filter = `blur(${Math.abs(depth)}px)`
         header.appendChild(p)
     }
