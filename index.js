db0b275816ebb447202d94015ecd03b93514ff90
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2023-08-22 16:45:34 -0400

    fixed scrolling somewhat

--- index.js
+++ index.js
@@ -44,12 +44,12 @@ window.addEventListener('load',()=>{
         if(track.dataset.mouseDownAt === "0") return;
         const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
         const maxDelta = window.innerWidth / 2;
-        const percentage = (mouseDelta / maxDelta) * 100;
+        const percentage = (mouseDelta / maxDelta) * 40;
         const nextPercentageUncon = parseFloat(track.dataset.prevPercentage) + percentage;
         const nextPercentage = Math.max(Math.min(nextPercentageUncon, 0), -100);
         track.dataset.percentage = nextPercentage;
         track.animate({
-            transform: `translate(${nextPercentage}%, 0%)`
+            transform: `translate(${nextPercentage * 4}%, 0%)`
         }, { duration: 1200, fill: "forwards" });
           
         for(const image of track.getElementsByClassName("image")) {
