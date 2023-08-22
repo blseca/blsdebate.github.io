41f95652c4e6f90c6cf2ac75e15cd66cef51b971
David Wang <73254509+ducc-coder@users.noreply.github.com>
2023-08-22 13:36:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -33,5 +33,33 @@ window.addEventListener('load',()=>{
             word.style.scale = size
         }
     })
+    const track = document.getElementById("images");
+    window.onmousedown = e => {
+
+        track.dataset.mouseDownAt = e.clientX;
+    }
+    window.onmousemove = e => {
+        if(track.dataset.mouseDownAt === "0") return;
+        const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+        const maxDelta = window.innerWidth / 2;
+        const percentage = (mouseDelta / maxDelta) * 100;
+        const nextPercentageUncon = parseFloat(track.dataset.prevPercentage) + percentage;
+        const nextPercentage = Math.max(Math.min(nextPercentageUncon, 0), -100);
+        track.dataset.percentage = nextPercentage;
+        track.animate({
+            transform: `translate(${nextPercentage}%, 0%)`
+        }, { duration: 1200, fill: "forwards" });
+          
+        for(const image of track.getElementsByClassName("image")) {
+            image.animate({
+              objectPosition: `${100 + nextPercentage}% center`
+            }, { duration: 1200, fill: "forwards" });
+        }
+    }
+    
+    window.onmouseup = () => {
+        track.dataset.mouseDownAt = "0";
+        track.dataset.prevPercentage = track.dataset.percentage;
+    }
 })
 
